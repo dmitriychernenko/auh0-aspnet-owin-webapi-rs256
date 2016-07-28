1d6471a7eb2ff4e435d0179df8d051ed2fccd7ec
Dmitriy Chernenko <dmitriy.chernenko@gmail.com>
2016-07-29 00:38:44 +0300

    Initial commit

---
+++ auh0-aspnet-owin-webapi-rs256/Controllers/ToDoController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace auh0_aspnet_owin_webapi_rs256.Controllers
+{
+    [Authorize]
+    public class ToDoController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok(new[] {"Buy milk", "Walk the dog"});
+        }
+    }
+}
---
+++ auh0-aspnet-owin-webapi-rs256/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Web.Configuration;
+using System.Web.Http;
+using auh0_aspnet_owin_webapi_rs256;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace auh0_aspnet_owin_webapi_rs256
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var auth0Issuer = WebConfigurationManager.AppSettings["Auth0Issuer"];
+            var auth0Audience = WebConfigurationManager.AppSettings["Auth0Audience"];
+
+            appBuilder.UseCors(CorsOptions.AllowAll);
+            var activeDirectoryFederationServicesBearerAuthenticationOptions = new ActiveDirectoryFederationServicesBearerAuthenticationOptions
+            {
+                MetadataEndpoint = $"{auth0Issuer.TrimEnd('/')}/wsfed/FederationMetadata/2007-06/FederationMetadata.xml",
+                TokenValidationParameters =
+                    new TokenValidationParameters
+                    {
+                        ValidAudience = auth0Audience,
+                        ValidIssuer = auth0Issuer,
+                        IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) => parameters.IssuerSigningTokens.FirstOrDefault()?.SecurityKeys?.FirstOrDefault()
+                    }
+            };
+
+            appBuilder.UseActiveDirectoryFederationServicesBearerAuthentication(activeDirectoryFederationServicesBearerAuthenticationOptions);
+
+            var httpConfiguration = new HttpConfiguration();
+            httpConfiguration.MapHttpAttributeRoutes();
+            httpConfiguration.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+            appBuilder.UseWebApi(httpConfiguration);
+        }
+    }
+}
